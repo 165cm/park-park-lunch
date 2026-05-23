ef2454efc8cc0c8b6cb110219f958a8e3b0dbc6f
Papazon <1650mm@gmail.com>
2026-05-23 17:02:45 +0900

    📱 スマホ優先の検索条件と店舗操作を改善

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "park-park-lunch-v12";
+const CACHE_NAME = "park-park-lunch-v13";
 const APP_SHELL = [
   "./",
-  "styles.css?v=3",
+  "styles.css?v=4",
   "config.js?v=1",
   "static-search.js?v=1",
-  "app.js?v=12",
+  "app.js?v=13",
   "manifest.webmanifest",
   "icon.svg"
 ];
